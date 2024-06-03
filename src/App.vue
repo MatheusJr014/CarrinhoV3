15241932f3a58b1cd99ee8f2a653d1233d0aaba5
Pedro Miguel <128000408+pedrocbp@users.noreply.github.com>
2024-06-03 19:44:44 -0300

    Update App.vue navbar

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,33 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/"><h1 class="logo" style="width: 44rem;">VIMA<span>STORE</span></h1></router-link>
+      <router-link class="navbar-brand" to="/"><h1 class="logo">VIMA<span>STORE</span></h1></router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-              <router-link class="nav-link" aria-current="page" to="/">Lançamentos</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" aria-current="page" to="/">Ofertas</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" aria-current="page" to="/">Masculino</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" aria-current="page" to="/">Feminino</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" aria-current="page" to="/contact">Contato</router-link>
-            </li>
-        </ul>        
+        <ul class="navbar-nav ms-auto me-auto central-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/">Lançamentos</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/">Ofertas</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/">Masculino</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/">Feminino</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/contact">Contato</router-link>
+          </li>
+        </ul>       
         <ul class="navbar-nav ms-auto">
-          <form class="d-flex" role="search" style="height: 2rem; margin-top: 3.5px; ">
-            <button class="btn btn-light" type="submit" style="background-color: #212529; border-color: #212529;"><span class="material-symbols-outlined" style="color: #C7C8C9;">search</span></button>
+          <form class="d-flex" role="search" style="height: 2rem; margin-top: 3.5px;">
+            <button class="btn btn-light" type="submit" style="background-color: #212529; border-color: #212529;">
+              <span class="material-symbols-outlined" style="color: #C7C8C9;">search</span>
+            </button>
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="border-radius: 50rem; width: 170px; height: 40px; text-align: center; margin-top: -5px;">
           </form>
           <li class="nav-item nav-car ms-auto">
@@ -77,5 +79,21 @@ let num = ref(0);
 
 .logo span {
   color: #fff;
+}
+.central-nav {
+  display: flex;
+  justify-content: center;
+  flex: 1;
+}
+
+.navbar-nav .nav-item {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+@media screen and (min-width: 1400px) {
+  .navbar-options {
+    margin-left: 250px; 
+  } 
 }
 </style>
